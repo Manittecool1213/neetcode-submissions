d3bf3742fc533c6a829509df3fab1f87426c5293
Manit Tanwar <57066306+Manittecool1213@users.noreply.github.com>
2026-08-11 18:59:20 +0530

    Add: valid-tree - submission-13

---
+++ Data Structures & Algorithms/valid-tree/submission-13.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n - 1:
+            return False
+
+        dsu = DSU(n)
+        for e1, e2 in edges:
+            if not dsu.union(e1, e2):
+                return False
+
+        return dsu.components == 1
+    
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.components = n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, a, b):
+        a, b = self.find(a), self.find(b)
+        if a != b:
+            if self.size[a] < self.size[b]:
+                a, b = b, a
+            self.size[a] += self.size[b]
+            self.parent[b] = a
+            self.components -= 1
+            return True
+        return False
