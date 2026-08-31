0f578761ce3073a226130575a66fdae2023d61bc
Manit Tanwar <57066306+Manittecool1213@users.noreply.github.com>
2026-08-31 21:07:26 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        DIRS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        ROWS, COLS = len(grid), len(grid[0])
+
+        queue = deque([])
+        visited = set()
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 0:
+                    queue.append((i, j))
+                    visited.add((i, j))
+        while queue:
+            cur_i, cur_j = queue.popleft()
+            for di, dj in DIRS:
+                i, j = cur_i + di, cur_j + dj
+                if 0 <= i < ROWS and 0 <= j < COLS and grid[i][j] != -1 and (i, j) not in visited:
+                    grid[i][j] = min(grid[i][j], grid[cur_i][cur_j] + 1)
+                    queue.append((i, j))
+                    visited.add((i, j))
